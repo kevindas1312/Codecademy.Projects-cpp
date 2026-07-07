f27085d6f6b09e3b0ddd0744685a4e11ce2a1877
kevindas1312 <dusty131207@gmail.com>
2026-07-08 00:23:02 +0800

    Enhance comments and documentation in tic-tac-toe.cpp
    Updated comments for clarity and added explanations for various functions and variables.

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include <ctime>
-using namespace std;
+#include <ctime> //library included for rand() function
+using namespace std; //to remove all use of std::
 
-void intro(int &turn){
+void intro(int &turn){ //function to display introduction
     cout << "Welcome to a game of Tic-Tac-Toe!\n";
     cout << "The following will be played on a 3x3 grid.\n";
     cout << "The board will consists of underscores which means that it has not been marked yet.\n";
@@ -13,35 +13,35 @@ void intro(int &turn){
     cout << "Player no. " <<turn<< " will begin the game!\n\n";
 }
 
-void character(){
+void character(){ //function to display with character must be used for each player
         cout << "\n\nPlayer no.1 must use /\n";
         cout << "\n\nPlayer no.2 must use O\n";
 }
 
 int main(){
-    string board[3][3];
-    int turn=0,row1=0,col1=0,row2=0,col2=0;
+    string board[3][3]; //initialization of array
+    int turn=0,row1=0,col1=0,row2=0,col2=0; //initializing variables that determine the position of characters for each user
     srand(time(NULL));
-    intro(turn);
+    intro(turn); //calling intro() function
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            board[i][j] = "_";
+            board[i][j] = "_"; //loop to set all elements of array to be "_"
         }
     }
 
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            cout << board[i][j];
+            cout << board[i][j]; //display array 
         }
         cout << endl;
     }
 
-    character();
+    character(); 
     cout << "Let the game begin!\n\n\n" ;
 
     int checker = 0;
-    string temp = " ";
+    string temp = " "; //set empty string variable to store element of array to check if there is double entry
     while(checker < 6){
         if(turn == 1){
             cout << "Row : ";
@@ -53,7 +53,7 @@ int main(){
             
             if(temp == "/" || temp == "O"){
                 cout << "Try again.\n\n";
-                checker--;
+                checker--; //so that the iterator of checker will remain constant if there is a false move
                 continue;
             }
             else{
@@ -62,10 +62,10 @@ int main(){
         }
         else{
             row2 = rand()%3;
-            col2 = rand()%3;
+            col2 = rand()%3; //rand() used to det position of character of NPC
             temp = board[row2][col2];
             if(temp == "/" || temp == "O"){
-                checker--;
+                checker--; //so that the iterator of checker will remain constant if there is a false move
                 continue;
             }
             else{
@@ -75,13 +75,13 @@ int main(){
 
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                cout << board[i][j];
+                cout << board[i][j]; //display array
             }
             cout << endl << endl;
         }
 
-        row1=0,col1=0,row2=0,col2=0;
-        if(turn == 1){
+        row1=0,col1=0,row2=0,col2=0; //reset all variables
+        if(turn == 1){ //IF BODY selection to change turns
             turn=2;
         }
         else{
@@ -107,6 +107,7 @@ int main(){
         }
     }
 
+    // 2. Check both diagonals
     if (board[0][0] == "/" && board[1][1] == "/" && board[2][2] == "/") {
         player1Won = true;
     }
@@ -114,7 +115,7 @@ int main(){
         player1Won = true;
     }
 
-
+    // 3. Final Verdict
     if (player1Won) {
         cout << "\n\n\n=======================" << endl;
         cout << "  Player 1 has won!!!  " << endl;
