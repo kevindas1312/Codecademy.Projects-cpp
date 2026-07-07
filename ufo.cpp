6f3927168091de1c0818dde229f92a32e04daff3
kevindas1312 <dusty131207@gmail.com>
2026-07-07 17:46:25 +0800

    Implement UFO guessing game in C++

---
+++ ufo.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void greet(){
+    cout <<"=============\n";
+    cout << "UFO: The Game\n";
+    cout << "=============\n";
+    cout << "Instructions: save your friend from alien abduction by guessing the letters in the codeword.\n\n";
+}
+
+void end_game(string answer, string codeword){
+    if (answer == codeword) {
+        cout << "Hooray! You saved your friend and earned a medal of honor!\n";
+    } 
+    else {
+        cout << "Oh no! The UFO just flew away with another person!\n";
+    }
+}
+
+void display_status(vector <char> incorrect, string answer){
+    cout << "Incorrect guesses: \t";
+    for(int i=0; i<incorrect.size();i++){
+        cout << incorrect[i] << " ";
+    }
+
+    cout << "Codeword: \t";
+    for(int j=0; j<answer.length();j++){
+        cout << answer[j] << " ";
+    }
+}
+
+void display_misses(int misses){
+    cout<< "\n__    __                          __                                             ______                       __                                         \n";
+    cout<<"|  |  |  |                       |  |                                           /      |                     |  |                                        \n";
+    cout<<"| $$| | $$ __    __  ______ ____  | $$____    ______    ______          ______  |  $$$$$$|      ______ ____   |$$  _______   _______   ______    _______ \n";
+    cout<<"| $$$|| $$|  |  |  ||      |   || $$    | /      |  /      |        /      | | $$_  |$$      |      |    | |  | /       | /       | /      |  /       \n";
+    cout<<"| $$$$| $$| $$  | $$| $$$$$$|$$$$|| $$$$$$$| |$$$$$$||  $$$$$$|      |  $$$$$$|| $$ |          | $$$$$$|$$$$|| $$|  $$$$$$$|  $$$$$$$|  $$$$$$||  $$$$$$$\n";
+    cout<<"| $$|$$ $$| $$  | $$| $$ | $$ | $$| $$  | $$| $$    $$| $$   |$$      | $$  | $$| $$$$          | $$ | $$ | $$| $$ |$$    |  |$$    | | $$    $$ |$$    | \n";
+    cout<<"| $$ |$$$$| $$__/ $$| $$ | $$ | $$| $$__/ $$| $$$$$$$$| $$            | $$__/ $$| $$            | $$ | $$ | $$| $$ _|$$$$$$| _|$$$$$$|| $$$$$$$$ _|$$$$$$\n";
+    cout<<"| $$  |$$$ |$$    $$| $$ | $$ | $$| $$    $$ $$     || $$             |$$    $$| $$            | $$ | $$ | $$| $$|       $$|       $$ |$$     ||       $$\n";
+    cout<<" |$$   |$$  |$$$$$$  |$$  |$$  |$$ |$$$$$$$   |$$$$$$$ |$$              |$$$$$$  |$$             |$$  |$$  |$$ |$$ |$$$$$$$  |$$$$$$$   |$$$$$$$ |$$$$$$$ \n";
+                                                                                                                                                          
+    cout << misses ;                                                                                                                                                                                                                                                                                                   
+}
+
+int main(){
+    string codeword = "codecademy";
+    string answer = "__________";
+    int misses = 0;
+    vector <char> incorrect;
+    bool guess = false;
+    char letter;
+
+    greet();
+
+    while(misses < 7 && answer != codeword){
+        display_misses(misses);
+        display_status(incorrect,answer);
+
+        cout << "\n\nPlease enter your guess: ";
+        cin >> letter;
+
+        for (int k = 0; k < codeword.length(); k++) {
+            if(letter == codeword[k]){
+                answer = letter;
+                guess = true;
+            }
+        }
+
+        if (guess) {
+            cout << "\nCorrect! You're closer to cracking the codeword.\n";
+        } else {
+            cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+            incorrect.push_back(letter);
+            misses++;
+        }
+
+        guess = false;
+    }
+
+    end_game(answer,codeword);
+
+}
